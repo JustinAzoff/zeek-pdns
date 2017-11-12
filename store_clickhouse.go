f43c6ca746175a820623fdc68659c60f961a985f
Justin Azoff <justin@bouncybouncy.net>
2017-11-11 21:54:30 -0500

    progress
    kind of have the tuples table updating

--- store_clickhouse.go
+++ store_clickhouse.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/jmoiron/sqlx"
@@ -49,6 +50,17 @@ CREATE TABLE IF NOT EXISTS filenames (
 	updated UInt64
   ) ENGINE = MergeTree(day, (filename), 8192);
 `}
+
+const tuples_temp_stmt = `
+CREATE TABLE tuples_temp (
+    query String,
+    type String,
+    answer String,
+    ttl UInt16,
+    first DateTime,
+    last DateTime,
+    count UInt64
+) ENGINE = Log`
 
 type CHStore struct {
 	conn *sqlx.DB
@@ -102,32 +114,48 @@ func (s *CHStore) Update(ar aggregationResult) (UpdateResult, error) {
 	var result UpdateResult
 	start := time.Now()
 
+	s.conn.Exec("DROP TABLE tuples_temp")
+	_, err := s.conn.Exec(tuples_temp_stmt)
+	if err != nil {
+		return result, errors.Wrap(err, "CHStore.Update failed to create temporary tuples table")
+	}
+	defer func() {
+		s.conn.Exec("DROP TABLE tuples_temp")
+	}()
+
 	tx, err := s.conn.Begin()
 	if err != nil {
 		return result, err
 	}
 
-	//insert into mt2 (key, value, first,last,total) select 'www.google.com', '1.2.3.4', minState(toDateTime(1498241729)),maxState(toDateTime(1498241729)), countState(cast(1 as UInt64));
-
-	stmt_tuples, err := tx.Prepare(`INSERT INTO tuples (query, type, answer, ttl, first, last, count)
-		SELECT ?, ?, ?,
-		anyLastState(cast(? as UInt16)),
-		minState(toDateTime(1498241729)),
-		maxState(toDateTime(1498241729)),
-		countState(cast(1 as UInt64))`)
+	stmt_tuples, err := tx.Prepare("INSERT INTO tuples_temp (query, type, answer, ttl, first, last, count) VALUES (?, ?, ?, ?, ?, ?, ?)")
 	if err != nil {
 		return result, err
 	}
 	for _, q := range ar.Tuples {
 		query := Reverse(q.query)
-		if _, err := stmt_tuples.Exec(query, q.qtype, q.answer, q.ttl); err != nil {
+		ttl, err := strconv.ParseInt(q.ttl, 10, 64)
+		if err != nil {
+			return result, errors.Wrap(err, "CHStore.Update failed")
+		}
+		if _, err := stmt_tuples.Exec(query, q.qtype, q.answer, ttl, int64(q.first), int64(q.last), int64(q.count)); err != nil {
 			return result, errors.Wrap(err, "CHStore.Update failed")
 		}
 
 	}
 
+	err = tx.Commit()
+	if err != nil {
+		return result, errors.Wrap(err, "CHStore.Update failed")
+	}
+
+	_, err = s.conn.Query(`INSERT INTO tuples (query, type, answer, ttl, first, last, count) SELECT query, type, answer, anyLastState(ttl), minState(first), maxState(last), countState(count) from tuples_temp group by query, type, answer`)
+	if err != nil {
+		//return result, errors.Wrap(err, "CHStore.Update failed to insert into tuples")
+	}
+
 	result.Duration = time.Since(start)
-	return result, tx.Commit()
+	return result, nil
 }
 
 func (s *CHStore) IsLogIndexed(filename string) (bool, error) {
@@ -167,7 +195,7 @@ func (s *CHStore) FindQueryTuples(query string) (tupleResults, error) {
 func (s *CHStore) FindTuples(query string) (tupleResults, error) {
 	tr := []tupleResult{}
 	rquery := Reverse(query)
-	err := s.conn.Select(&tr, "SELECT * FROM tuples WHERE query = ? OR answer = ? ORDER BY query, answer", rquery, query)
+	err := s.conn.Select(&tr, "SELECT query, type, answer, minMerge(first) as first, maxMerge(last) as last, countMerge(count) as count from tuples WHERE query = ? OR answer = ? group by query, type, answer ORDER BY query, answer", rquery, query)
 	reverseQuery(tr)
 
 	return tr, err
@@ -182,7 +210,8 @@ func (s *CHStore) LikeTuples(query string) (tupleResults, error) {
 func (s *CHStore) FindIndividual(value string) (individualResults, error) {
 	rvalue := Reverse(value)
 	tr := []individualResult{}
-	err := s.conn.Select(&tr, "SELECT * FROM individual WHERE (which='A' AND value = ?) OR (which='Q' AND value = ?) ORDER BY value", value, rvalue)
+	err := s.conn.Select(&tr, `SELECT which, value, minMerge(first) as first, maxMerge(last) as last, countMerge(count) as count from individual
+	WHERE (which='A' AND value = ?) OR (which='Q' AND value = ?) group by which, value ORDER BY value`, value, rvalue)
 	reverseValue(tr)
 	return tr, err
 }
