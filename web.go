d3b84b6d9a1f386c9c2237477989ecd459b0320b
Justin Azoff <justin@azoff.dev>
2021-04-04 13:16:03 -0400

    replace JS SPA ui with simpler html form

--- web.go
+++ web.go
@@ -1,15 +1,17 @@
-//go:generate go-bindata -prefix "assets/" -pkg main -o assets.go assets/...
 package main
 
 import (
-	"bytes"
+	"embed"
 	"encoding/json"
-	"io"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/gorilla/mux"
 )
+
+//go:embed template
+var content embed.FS
 
 type pdnsHandler struct {
 	s Store
@@ -64,17 +66,45 @@ func (h *pdnsHandler) handleSearchIndividual(w http.ResponseWriter, req *http.Re
 	json.NewEncoder(w).Encode(recs)
 }
 
-// http://rachbelaid.com/embedding-assets-in-go-project/
-func static_handler(rw http.ResponseWriter, req *http.Request) {
-	var path string = req.URL.Path
-	if path == "" {
-		path = "index.html"
+type Results struct {
+	Query      string
+	Exact      bool
+	Individual individualResults
+	Tuples     tupleResults
+	Error      error
+}
+
+func (h *pdnsHandler) handleUI(w http.ResponseWriter, req *http.Request) {
+	var err error
+	var res Results
+	res.Query = req.FormValue("query")
+	res.Exact = req.FormValue("exact") == "on"
+	if res.Query != "" {
+		if res.Exact {
+			res.Individual, err = h.s.FindIndividual(res.Query)
+			if err != nil {
+				res.Error = err
+			}
+			res.Tuples, err = h.s.FindTuples(res.Query)
+			if err != nil {
+				res.Error = err
+			}
+		} else {
+			res.Individual, err = h.s.LikeIndividual(res.Query)
+			if err != nil {
+				res.Error = err
+			}
+			res.Tuples, err = h.s.LikeTuples(res.Query)
+			if err != nil {
+				res.Error = err
+			}
+		}
 	}
-	if bs, err := Asset(path); err != nil {
-		rw.WriteHeader(http.StatusNotFound)
-	} else {
-		var reader = bytes.NewBuffer(bs)
-		io.Copy(rw, reader)
+
+	var t = template.Must(template.New("index.html").ParseFS(content, "template/index.html"))
+	err = t.Execute(w, res)
+	if err != nil {
+		log.Printf("Error rendering template: %v", err)
 	}
 }
 
@@ -88,8 +118,7 @@ func startWeb(s Store, bind string) {
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/ui/", http.StatusSeeOther)
 	})
-	r.Handle("/ui/", http.StripPrefix("/ui/", http.HandlerFunc(static_handler)))
-	r.Handle("/ui/{path}", http.StripPrefix("/ui/", http.HandlerFunc(static_handler)))
+	r.HandleFunc("/ui/", h.handleUI)
 
 	http.Handle("/", r)
 
